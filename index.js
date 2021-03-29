c22edefca27bcfd0ff57f1a64c6bee96f53f1ac8
luckmer <woszczo04@gmail.com>
2021-03-29 16:12:40 +0200

    chess board

---
+++ index.js
@@ -0,0 +1,58 @@
+const board = document.querySelector(".board");
+const boardLetters = document.querySelector(".letters");
+const boardNumbers = document.querySelector(".numbers");
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+let possibleClick = ["pawn", "rook", "knight", "bishop", "queen", "king"];
+let FindDataSet = ["b", "r", "k", "p"];
+let detectRook = ["x", "y", "z", "x"];
+let GlobalColisionSelector = "";
+let CalculateXXaxis = -1;
+let ChessBoard = 8;
+let widthX = 9;
+let widthY = 8;
+let ChessBack = 8;
+let count = 8;
+let DetectQueen = false;
+let index = 0;
+let num = 1;
+let ChessWidth = 8;
+let black = false;
+const table = [];
+let TakeColiderId;
+
+const CreateChessControls = () => {
+  for (let i = 0; i < ChessBack; i++) {
+    let letter = document.createElement("li");
+    letter.textContent = letters[i];
+    boardLetters.appendChild(letter);
+    let numbers = document.createElement("li");
+    numbers.textContent = num++;
+    boardNumbers.appendChild(numbers);
+  }
+};
+
+CreateChessControls();
+
+const CreateChessBoard = () => {
+  for (let i = 1; i <= ChessBoard * ChessBoard; i++) {
+    const square = document.createElement("div");
+    square.id = i;
+    square.dataset.id = 1;
+    if (black) {
+      square.classList.add("square", "black");
+      index++;
+      black = !black;
+    } else {
+      square.classList.add("square", "white");
+      index++;
+      black = !black;
+    }
+    table.push(square);
+    board.appendChild(square);
+    if (index === 8) {
+      black = !black;
+      index = 0;
+    }
+  }
+};
+CreateChessBoard();
